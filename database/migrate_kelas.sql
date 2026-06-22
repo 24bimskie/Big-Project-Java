b35d9cd24183602402991f3e085ecd3bbfeebea6
Danang <ainalwckk11@gmail.com>
2026-06-22 22:05:45 +0700

    update admin

---
+++ database/migrate_kelas.sql
@@ -0,0 +1,22 @@
+-- ============================================================
+-- MIGRASI: Tambah data dummy tabel kelas
+-- ============================================================
+
+INSERT INTO `kelas` (`Prodi`, `kelas`) VALUES
+('Teknik Informatika', '1A'),
+('Teknik Informatika', '1B'),
+('Teknik Informatika', '2A'),
+('Teknik Informatika', '2B'),
+('Teknik Informatika', '3A'),
+('Teknik Informatika', '3B'),
+('Sistem Informasi', '1A'),
+('Sistem Informasi', '1B'),
+('Sistem Informasi', '2A'),
+('Sistem Informasi', '2B'),
+('Sistem Informasi', '3A'),
+('Manajemen Informatika', '1A'),
+('Manajemen Informatika', '2A'),
+('Teknik Komputer', '1A'),
+('Teknik Komputer', '2A'),
+('Sistem Komputer', '1A'),
+('Sistem Komputer', '2A');
