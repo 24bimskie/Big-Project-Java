f6ac763a0155ae1a6693c5a8a458ffaecfbf5bec
Danang <ainalwckk11@gmail.com>
2026-06-23 22:27:01 +0700

    Update dashboard admin

--- src/app/Main.java
+++ src/app/Main.java
@@ -28,7 +28,6 @@ public class Main extends Application {
 
             primaryStage.setTitle("Sistem Absensi Mahasiswa");
             primaryStage.setScene(scene);
-            primaryStage.setResizable(false);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
